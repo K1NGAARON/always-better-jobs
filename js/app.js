3749dfb0f2502fcccb53e85b1a89a0d2576a0bc3
K1NGAARON <gerardaaron1997@gmail.com>
2023-05-09 11:07:40 +0200

    changes

--- js/app.js
+++ js/app.js
@@ -1,27 +1,3 @@
-const job = [
-    {
-        title: "Content Creator met strategisch inzicht",
-        body: "Dit is de body van job uno",
-        img: "https://start.alwaysbetter.be/ambassador-traject/img/ab-image.jpg",
-        tag: "marketing",
-        url: "",
-    },
-    {
-        title: "Content Creator met strategisch inzicht",
-        body: "Dit is de body van job uno",
-        img: "https://start.alwaysbetter.be/ambassador-traject/img/ab-image.jpg",
-        tag: "coach",
-        url: "",
-    },
-    {
-        title: "Content Creator met strategisch inzicht",
-        body: "Dit is de body van job uno",
-        img: "https://start.alwaysbetter.be/ambassador-traject/img/ab-image.jpg",
-        tag: "admin",
-        url: "",
-    },
-];
-
 // STICKY LARGE MENU
 $(document).scroll(function () {
     const header = $('.header');
@@ -55,61 +31,4 @@ $('.small-menu-content .nav').click(closeMenu);
 
 $("#menu-toggle").click(function() {
     $(".small-menu-wrapper").toggle("active");
-});
-
-function filterContent(e) {
-    // Toggle Active Class on Filter
-    $('.filter-btn').removeClass('active');
-    $(this).addClass('active');
-
-    $('.job-openings .card').fadeOut();
-
-    // Selected Tag
-    let activeFilter = $(this).attr('id');
-
-    if (activeFilter === 'all') {
-        $('.job-openings .card').fadeIn();
-    } else {
-        $('.' + activeFilter).fadeIn();
-    }
-};
-
-
-function createCards(e) {
-    const target = document.querySelector('#jobs-openings');
-
-    if (target) {
-        const cards = job.map(job => {
-            const tagWithSpaces = job.tag.replace(/-/g, ' ');
-
-
-            return `
-                <div class="card ${job.date} ${job.category} ${job.tag}">
-                    <div class="card-image">
-                        <img src="${job.img}" alt="${job.title}">
-                        <div class="overlay"></div>
-                    </div>
-                    <div class="card-content">
-                        <p class="tag">
-                            ${tagWithSpaces}
-                        </p>
-                        <h5>
-                            ${job.title}
-                        </h5>
-                        <a href="/posts/${job.link}" class="btn ghost no-border">
-                            Lees meer <i class="fa-solid fa-chevron-right"></i>
-                        </a>
-                    </div>
-                </div>
-            `;
-        }).join('');
-    
-        target.innerHTML = cards;
-    }
-};
-
-
-$(document).ready(function() {
-    createCards();
-    $('.filter-btn').click(filterContent);
 });
